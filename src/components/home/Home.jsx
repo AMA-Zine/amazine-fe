2b5585a3859d664546e0db96f0c7d9869399ca51
Patrick Wilson <pmwilson333@gmail.com>
2020-11-18 16:51:10 -0800

    articles pulling data from backend

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -10,17 +10,35 @@ export default function Home() {
   const { wine } = useWine();
   const { dessert } = useDessert();
   const { natGeo } = useNatGeo();
+  const subThreads = threads.slice(5);
 
-  console.log('here', threads);
+  console.log('threads', threads);
+  console.log('recipe', recipe);
+  console.log('wine', wine);
+  console.log('dessert', dessert);
+  console.log('natGeo', natGeo);
+
+  const subThreadList = subThreads.map((currentThread) => (
+    <>
+      <article className={styles.listArticle}>
+        <img src="http://placekitten.com/150/200"></img>
+        <section>
+          <h4 className=
+            {styles.titleFont}>{currentThread && currentThread.title}
+          </h4>
+          <p className={styles.bodyFont}>author</p>
+        </section>
+      </article>
+      <hr />
+    </>
+  ));
 
   return (
     <div className={`${styles.headerSpace} ${styles.bottomSpace}`}>
       <hr />
       <article className={styles.bannerArticle}>
-        <img src="http://placekitten.com/300/50" alt="kitten"></img>
-        <h2 className={styles.normalGap}>
-          Everything You Need to Know About Cool Things and Stuff
-        </h2>
+        <img src="http://placekitten.com/300/50" alt={natGeo.alt}></img>
+        <h2 className={styles.normalGap}>National Geographic Picture</h2>
       </article>
       <hr />
       <div className={styles.articleGrid}>
@@ -28,8 +46,8 @@ export default function Home() {
           ${styles.featured}
         `}>
           <img src="http://placekitten.com/600/400" alt="kitten"></img>
-          <h3 className={styles.titleFont}>Featured Article</h3>
-          <p className={styles.bodyFont}>Article Description</p>
+          <h3 className={styles.titleFont}>{threads[0] && threads[0].title}</h3>
+          <p className={styles.bodyFont}>reporter</p>
         </article>
         <article className={`
           ${styles.article} 
@@ -37,8 +55,8 @@ export default function Home() {
           ${styles.columnLeft}
         `}>
           <img src="http://placekitten.com/300/250" alt="kitten"></img>
-          <h3 className={styles.titleFont}>Left Top Article</h3>
-          <p className={styles.bodyFont}>Article Description</p>
+          <h3 className={styles.titleFont}>{threads[1] && threads[1].title}</h3>
+          <p className={styles.bodyFont}>reporter</p>
         </article>
         <article className={`
           ${styles.article} 
@@ -46,8 +64,8 @@ export default function Home() {
           ${styles.columnLeft}
         `}>
           <img src="http://placekitten.com/300/250" alt="kitten"></img>
-          <h3 className={styles.titleFont}>Left Bottom Article</h3>
-          <p className={styles.bodyFont}>Article Description</p>
+          <h3 className={styles.titleFont}>{threads[2] && threads[2].title}</h3>
+          <p className={styles.bodyFont}>reporter</p>
         </article>
         <article className={`
           ${styles.article} 
@@ -55,8 +73,8 @@ export default function Home() {
           ${styles.columnRight}
         `}>
           <img src="http://placekitten.com/300/250" alt="kitten"></img>
-          <h3 className={styles.titleFont}>Right Top Article</h3>
-          <p className={styles.bodyFont}>Article Description</p>
+          <h3 className={styles.titleFont}>{threads[3] && threads[3].title}</h3>
+          <p className={styles.bodyFont}>reporter</p>
         </article>
         <article className={`
           ${styles.article} 
@@ -64,8 +82,8 @@ export default function Home() {
           ${styles.columnRight}
         `}>
           <img src="http://placekitten.com/300/250" alt="kitten"></img>
-          <h3 className={styles.titleFont}>Right Bottom Article</h3>
-          <p className={styles.bodyFont}>Article Description</p>
+          <h3 className={styles.titleFont}>{threads[4] && threads[4].title}</h3>
+          <p className={styles.bodyFont}>reporter</p>
         </article>
       </div>
 
@@ -76,53 +94,30 @@ export default function Home() {
           ${styles.lowerLeft}
         `}>
           <img src="http://placekitten.com/150/100"></img>
-          <h4 className={styles.titleFont}>Article</h4>
-          <p className={styles.bodyFont}>Article Description</p>
+          <h4 className={styles.titleFont}>Recipe</h4>
+          <p className={styles.bodyFont}>Recipe Description</p>
         </article>
         <article className={`
           ${styles.article} 
           ${styles.lowerCenter}
         `}>
           <img src="http://placekitten.com/150/100"></img>
-          <h4 className={styles.titleFont}>Article</h4>
-          <p className={styles.bodyFont}>Article Description</p>
+          <h4 className={styles.titleFont}>Wine</h4>
+          <p className={styles.bodyFont}>Wine Description</p>
         </article>
         <article className={`
           ${styles.article} 
           ${styles.lowerRight}
         `}>
           <img src="http://placekitten.com/150/100"></img>
-          <h4 className={styles.titleFont}>Article</h4>
-          <p className={styles.bodyFont}>Article Description</p>
+          <h4 className={styles.titleFont}>Dessert</h4>
+          <p className={styles.bodyFont}>Dessert Description</p>
         </article>
       </div>
       <hr />
 
       <div>
-        <article className={styles.listArticle}>
-          <img src="http://placekitten.com/150/200"></img>
-          <section>
-            <h4 className={styles.titleFont}>Article</h4>
-            <p className={styles.bodyFont}>Article Description</p>
-          </section>
-        </article>
-        <hr />
-        <article className={styles.listArticle}>
-          <img src="http://placekitten.com/150/200"></img>
-          <section>
-            <h4 className={styles.titleFont}>Article</h4>
-            <p className={styles.bodyFont}>Article Description</p>
-          </section>
-        </article>
-        <hr />
-        <article className={styles.listArticle}>
-          <img src="http://placekitten.com/150/200"></img>
-          <section>
-            <h4 className={styles.titleFont}>Article</h4>
-            <p className={styles.bodyFont}>Article Description</p>
-          </section>
-        </article>
-        <hr />
+        {subThreads.length > 0 && subThreadList}
       </div>
     </div>
   );
