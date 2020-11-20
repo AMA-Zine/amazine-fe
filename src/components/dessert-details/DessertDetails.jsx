2ce40f1db57793377941328641f8107f093a667f
SarahRector <rector.sarah.r@gmail.com>
2020-11-19 20:31:18 -0800

    added food styling

--- src/components/dessert-details/DessertDetails.jsx
+++ src/components/dessert-details/DessertDetails.jsx
@@ -1,20 +1,21 @@
+/* eslint-disable max-len */
 import React from 'react';
 import { useDessert } from '../../hooks/recipes';
 import styles from '../app/styles.css';
+import foodstyles from '../recipe-details/foodStyles.css';
 
 const DessertDetail = () => {
   const { dessert } = useDessert();
-  console.log(dessert);
 
   // eslint-disable-next-line space-in-parens
   const summary = dessert?.summary?.replace( /(<([^>]+)>)/ig, '');
 
   return (
-    <div className={styles.headerSpace}>
+    <div className={`${styles.headerSpace} ${foodstyles.foodItem}`}>
       <h1>{dessert.title}</h1>
-      <a href={dessert.linkSrc}>Click Here for Dessert Recipe</a>
+      <a className={foodstyles.link} href={dessert.linkSrc}>Click Here for Dessert Recipe</a>
       <img src={dessert.image} alt={dessert.title} />
-      <p>{summary}</p>
+      <p className={foodstyles.foodText}>{summary}</p>
     </div>
   );
 };
